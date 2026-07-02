043f1eecbd4a2142c0b04a485d3a8ae41cc9995f
lethanh2006 <thanhld2006.ptit@gmail.com>
2026-07-02 16:57:19 +0700

    giao diện apd

--- pages/_app.js
+++ pages/_app.js
@@ -11,13 +11,13 @@ export default ({ Component, pageProps }) => (
     <Layout>
       <Modal />
       <DefaultSeo
-        title="HỌC VIỆN CÔNG NGHỆ BƯU CHÍNH VIỄN THÔNG CỔNG THÔNG TIN ĐÀO TẠO"
-        description="Trang Web cung cấp thông tin về Học viện Công nghệ Bưu chính Viễn Thông"
+        title="HỌC VIỆN CHÍNH SÁCH VÀ PHÁT TRIỂN CỔNG THÔNG TIN ĐÀO TẠO"
+        description="Trang Web cung cấp thông tin về Học viện Chính sách và Phát triển"
         openGraph={{
           type: 'website',
           locale: 'vi_VN',
           url: 'https://tuyensinhptit.aisenote.com/',
-          site_name: 'HỌC VIỆN CÔNG NGHỆ BƯU CHÍNH VIỄN THÔNG CỔNG THÔNG TIN ĐÀO TẠO',
+          site_name: 'HỌC VIỆN CHÍNH SÁCH VÀ PHÁT TRIỂN CỔNG THÔNG TIN ĐÀO TẠO',
           images: [
             {
               url: 'https://portal.ptit.edu.vn/wp-content/uploads/2019/01/1-10.jpg',
